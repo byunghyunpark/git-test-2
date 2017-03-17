0ebad622dee9a9449c67699feeb8207a5241bf0b
byunghyunpark <openmind8735@gmail.com>
2017-03-17 17:53:40 +0900

    elb test

--- django_app/blog/urls.py
+++ django_app/blog/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+
+from blog import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'blog/', include('blog.urls', namespace='blog')),
+    url(r'^$', views.index, name='index'),
 ]
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    result = [i*2 for i in range(100000)]
+    result2 = []
+    for i, number in enumerate(result):
+        result2.append((i, number))
+    context = {
+        "result": result2,
+    }
+    return render(request, 'index.html', context)
